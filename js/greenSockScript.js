cf946bc1514a1a7ea3a862fb3c282fe9013bf9c0
IONIQ6000 <50470441+IONIQ6000@users.noreply.github.com>
2022-09-04 14:41:01 -0400

    v0.2.387897

--- js/greenSockScript.js
+++ js/greenSockScript.js
@@ -1,5 +1,5 @@
 gsap.to(".hscrollFromLeft", {
-    translateX: "30%",
+    translateX: "35%",
     scrollTrigger: {
         trigger: ".hscrollFromLeft",
         scrub: 1,
@@ -7,61 +7,107 @@ gsap.to(".hscrollFromLeft", {
 });
 
 gsap.to(".hscrollFromRight", {
-    translateX: "-30%",
+    translateX: "-35%",
     scrollTrigger: {
         trigger: ".hscrollFromRight",
         scrub: 1,
     }
 });
+
+gsap.from('.home-ani-1', {
+    duration: 1.5,
+    delay: .4,
+    opacity: 0,
+    x: -500,
+});
+gsap.from('.home-ani-2', {
+    duration: 1.5,
+    delay: .6,
+    opacity: 0,
+    x: -500,
+});
+gsap.from('.home-ani-3', {
+    duration: 1.5,
+    delay: .8,
+    opacity: 0,
+    x: -500,
+});
+gsap.from('.home-ani-4', {
+    duration: 1.5,
+    delay: 1,
+    opacity: 0,
+    x: -500,
+});
+gsap.from('.home-ani-5', {
+    duration: 1.5,
+    delay: 1.2,
+    opacity: 0,
+    x: -500,
+});
+
 gsap.from('.logo', {
     duration: 1,
+    delay: 1.4,
+    opacity: 0,
+    x: -300,
+});
+
+gsap.from('.home-img', {
+    duration: 1.7,
     delay: .4,
-    opacity:0,
-    x:-100,
+    opacity: 0,
+    x: 500,
 });
 
 gsap.from('.navlist', {
     duration: 1,
-    delay: .4,
-    opacity:0,
-    y:100,
+    delay: 1.4,
+    opacity: 0,
+    x: 300,
+
 });
-gsap.from('#about', {
-    duration: 1,
-    delay: .7,
-    opacity:0,
-    y:100,
+
+
+gsap.from('.about-text', {
+    duration: 1.5,
+    delay: .6,
+    opacity: 0,
+    x: 300,
+    scrollTrigger: {
+        trigger: '#about',
+    }
+});
+gsap.from('.about-img', {
+    duration: 1.5,
+    delay: .6,
+    opacity: 0,
+    x: -300,
     scrollTrigger: {
         trigger: '#about',
     }
 });
 
-
-
 gsap.from('#portfolio', {
-    duration: 1,
-    delay: .7,
-    opacity:0,
-    x:100,
+    duration: 1.5,
+    delay: .6,
+    opacity: 0,
     scrollTrigger: {
         trigger: '#portfolio',
     }
 });
 gsap.from('#contact', {
-    duration: 1,
-    delay: .7,
-    opacity:0,
-    x:-100,
+    duration: 1.5,
+    delay: .6,
+    opacity: 0,
     scrollTrigger: {
         trigger: '#contact',
     }
 });
 
 gsap.from('.ends', {
-    duration: 1,
-    delay: .7,
-    opacity:0,
-    x:100,
+    duration: 1.5,
+    delay: .6,
+    opacity: 0,
     scrollTrigger: {
         trigger: '.ends',
     }
