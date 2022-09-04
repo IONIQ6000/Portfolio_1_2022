bb83e86e80070369d9ffd557a0d8964c6ef7c137
IONIQ6000 <50470441+IONIQ6000@users.noreply.github.com>
2022-09-03 21:31:38 -0400

    v0.1.34

--- js/greenSockScript.js
+++ js/greenSockScript.js
@@ -1,49 +1,60 @@
 gsap.to(".hscrollFromLeft", {
-    translateX: "20%",
+    translateX: "30%",
     scrollTrigger: {
         trigger: ".hscrollFromLeft",
-        scrub: .5,
+        scrub: 1,
     }
 });
 
 gsap.to(".hscrollFromRight", {
-    translateX: "-20%",
+    translateX: "-30%",
     scrollTrigger: {
         trigger: ".hscrollFromRight",
-        scrub: .5,
+        scrub: 1,
+    }
+});
+gsap.from('.logo', {
+    duration: 1,
+    delay: .4,
+    opacity:0,
+    x:-100,
+});
+
+gsap.from('.navlist', {
+    duration: 1,
+    delay: .4,
+    opacity:0,
+    y:100,
+});
+gsap.from('#about', {
+    duration: 1,
+    delay: .7,
+    opacity:0,
+    y:100,
+    scrollTrigger: {
+        trigger: '#about',
     }
 });
 
 
-TweenMax.from('.header-1', .4,{
-    delay: .1,
-    opacity:0,
-    y:80,
 
-});
-TweenMax.from('.header-2', .4,{
-    delay: .4,
-    opacity:0,
-    x:80,
-});
-TweenMax.from('.header-3', .4,{
-    delay: .4,
-    opacity:0,
-    x:-80,
-});
-TweenMax.from('.header-4', .4,{
-    delay: .4,
-    opacity:0,
-    y:80,
-});
-TweenMax.from('.header-5', .4,{
+gsap.from('#portfolio', {
+    duration: 1,
     delay: .7,
     opacity:0,
-    x:80,
+    x:100,
+    scrollTrigger: {
+        trigger: '#portfolio',
+    }
 });
-TweenMax.from('.sub-header-1', .4,{
-    delay: 1,
+gsap.from('#contact', {
+    duration: 1,
+    delay: .7,
     opacity:0,
-    x:80,
+    x:-100,
+    scrollTrigger: {
+        trigger: '#contact',
+    }
 });
+
 
